a84e04e167ac10469b2d8eae51f24c956d1ac791
Sanjana24kairamkonda <96222161+Sanjana24kairamkonda@users.noreply.github.com>
2025-03-10 12:06:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,8 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
+
+variable "gcp_sa_key" {
+  description = "The Google Cloud Service Account key in JSON format"
+  type        = string
+}
